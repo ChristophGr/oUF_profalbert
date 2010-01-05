a0fe0d4bd0dc9439c1c3493396018d30e332e49e
Christoph Gritschenberger <profalbert@gmail.com>
2010-01-05 02:07:53 +0100

    remove debug-output and commented code

--- vehicle2.lua
+++ vehicle2.lua
@@ -7,9 +7,6 @@ local UPDATE_TIME = 3
 local vehicleDelayFrame = CreateFrame('Frame')
 local affectedFrames = {}
 
-
--- local function print() end
-
 local function updateAllElements(frame)
 	for _, v in ipairs(frame.__elements) do
 		v(frame, 'UpdateElement', frame.unit)
@@ -18,17 +15,14 @@ end
 
 local function forceUpdate(self, elapsed)
 	for k,v in pairs(affectedFrames) do
-		--print("updating frame ", k:GetName())
 		if k.unit and UnitExists(k.unit) then
 			updateAllElements(k)
 			affectedFrames[k] = nil
-			--print(k.unit, " existed after ", elapsed)
 			return
 		end
 
 		local afterUpdate = v - elapsed
 		if afterUpdate < 0 then
-			--print("updating finished for ", k:GetName(), " (", afterUpdate, ")")
 			afterUpdate = nil
 		end
 		affectedFrames[k] = afterUpdate
@@ -45,28 +39,15 @@ local Update = function(self)
 	local unit = self.unit
 	local normalUnit = self:GetAttribute("unit")
 	local modunit = SecureButton_GetModifiedUnit(self)
-	--print("unit: ", unit, "; modunit: ", modunit)
 	if modunit ~= unit then
 		self.unit = modunit
-		if unit == normalUnit then
-			-- not in vehicle
+		if unit == normalUnit then -- not in vehicle
 			self.vehicleUnit = nil
 		else
 			self.vehicleUnit = modunit
 		end
 		affectedFrames[self] = UPDATE_TIME
 		vehicleDelayFrame:Show()
-	--[[elseif modunit == unit and self.vehicleUnit then
-		self.vehicleUnit = nil
-		self.unit = unit
-		affectedFrames[self] = UPDATE_TIME
-		vehicleDelayFrame:Show()--]]
-		print("updating ", self:GetName())
-	--[[else
-		print("skipping updating ", self:GetName())
-		print("unit: ", unit)
-		print("modunit: ", modunit)
-		print("vehicleUnit: ", self.vehicleUnit)--]]
 	end
 	
 	if unit == "player" then
@@ -80,7 +61,6 @@ local function attrChanged(self, name, value)
 	if self.unit and modunit ~= self.unit then
 		self.vehicleUnit = modunit
 		self.unit = modunit
-		--print(self:GetName(), " HAHAHA, gotcha OnAttributeChanged....")
 	end
 end
 
