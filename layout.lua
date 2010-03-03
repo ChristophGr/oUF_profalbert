0490b8e04586e444e9f6f07d0f262aa777401d8c
Christoph Gritschenberger <profalbert@gmail.com>
2010-03-04 00:02:48 +0100

    use SpellRange instead of Range

--- layout.lua
+++ layout.lua
@@ -928,7 +928,7 @@ local function setStyle(settings, self, unit)
 	end -- if unit-find
 
 	if settings["range-fade"] then
-		self.Range = true
+		self.SpellRange = true
 		self.inRangeAlpha = 1.0
 		self.outsideRangeAlpha = 0.6
 	end
