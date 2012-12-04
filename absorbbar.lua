962126fb8ae6f05aafc9841b5f66b85482aef3e1
Christoph Gritschenberger <profalbert@gmail.com>
2012-12-04 14:16:28 +0100

    fix absorb-spell-ids

--- absorbbar.lua
+++ absorbbar.lua
@@ -1,15 +1,29 @@
 local oUF = _G.oUF
 assert(oUF, "oUF not found")
 
+local ABSORB_SPELLS = {
+	[GetSpellInfo(112048)] = true, -- Shield Barrier
+	[GetSpellInfo(20925)] = true, -- Sacred Shield
+	[GetSpellInfo(66099)] = true, -- Power Word: Shield
+	[GetSpellInfo(47515)] = true, -- Divine Aegis
+	[GetSpellInfo(11426)] = true, -- Ice Barrier
+	[GetSpellInfo(56778)] = true, -- Mana Shield
+	[GetSpellInfo(7812)] = true, -- Sacrifice
+	-- local AB_MW = 'Mage Ward'
+	[GetSpellInfo(116631)] = true -- Colossus (Weapon Enchant)
+	[GetSpellInfo(118604)] = true -- Guard (Monk)
+	[GetSpellInfo(116849)] = true -- Life Cocoon (Monk)
+}
+
 local newdata = {}
 local function updateAbsorbInfo(unit, data)
 	local totalamount = 0
 	local totalmax = 0
 	local result = newdata
 	for i = 1,40 do
-		local _, _, icon, count, _, duration, expirationTime, unitCaster, _, 
+		local name, _, icon, count, _, duration, expirationTime, unitCaster, _, 
 _, spellId, _, _, _, amount = UnitBuff(unit, i)
-		if amount then
+		if ABSORB_SPELLS[name] then
 			if data[spellId] then
 				newdata[spellId] = data[spellId]
 				data[spellId] = nil
@@ -30,7 +44,7 @@ local Update = function(self, event, unit)
 	local absorb = self.Absorb
 	local amount, max, data = updateAbsorbInfo(unit, absorb.data)
 	absorb.data = data
-	if amount then
+	if amount > 0 then
 		absorb:SetMinMaxValues(0, max)
 		absorb:SetValue(amount)
 	else
